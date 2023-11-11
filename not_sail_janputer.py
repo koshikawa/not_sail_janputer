482049e400068d6d293f83f0a640f4f27a3b8180
Koshikawa Yasunori <y-koshikawa@skyhobbit.co.jp>
2023-11-11 22:38:07 +0900

    リファクタリング

--- not_sail_janputer.py
+++ not_sail_janputer.py
@@ -4,22 +4,19 @@ import math
 
 def get_score(fu, han, dealer):
     """ 得点を返す """
-    child_score = fu * 2 ** (han + 4)
-
-    if ((fu == 20 or fu == 25) and han <= 1):
+    if (fu in (20, 25) and han <= 1):
         return 0
-    if child_score >= 8000:
-        child_score = 8000
-    if han >= 6:
+    child_score = min(fu * 2 ** (han + 4), 8000)
+    if han in (6, 7):
         child_score = 12000
-    if han >= 8:
+    if han in (8, 9, 10):
         child_score = 16000
-    if han >= 11:
+    if han in (11, 12):
         child_score = 24000
     if han >= 13:
         child_score = 32000
 
-    return child_score + (child_score * dealer * 0.5)
+    return child_score * (1 + dealer * 0.5)
 
 for dealer in (False, True):
     for fu in (list(range(20, 80, 2)) + [25]):
