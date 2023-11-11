2b52f8f509de1c3563f4960b0a2613d9a3dd7920
Koshikawa Yasunori <y-koshikawa@skyhobbit.co.jp>
2023-11-11 22:23:11 +0900

    デグレ修正

--- not_sail_janputer.py
+++ not_sail_janputer.py
@@ -2,29 +2,29 @@
 """ not sail janputer """
 import math
 
-def score(fu, han, dealer):
+def get_score(fu, han, dealer):
+    """ 得点を返す """
+    child_score = fu * 2 ** (han + 4)
+
     if ((fu == 20 or fu == 25) and han <= 1):
         return 0
-    score = fu * 2 ** (han + 4) * (1 + dealer / 2)
-    #score = math.ceil((math.ceil(fu / 10) * 10 if fu != 25 else 25) * 2 ** (han + 4) * (1 + dealer / 2) / 100) * 100 # pylint: disable=C0301
-    if (dealer and score < 12000 or not dealer and score < 8000):
-        return score
-    else:
-        if han <= 5:
-            return 8000 + (dealer * 4000)
-        elif han <= 7:
-            return 12000 + (dealer * 6000)
-        elif han <= 10:
-            return 16000 + (dealer * 8000)
-        elif han <= 12:
-            return 24000 + (dealer * 12000)
-        else:
-            return 32000 + (dealer * 16000)
+    if child_score >= 8000:
+        child_score = 8000
+    if han >= 6:
+        child_score = 12000
+    if han >= 8:
+        child_score = 16000
+    if han >= 11:
+        child_score = 24000
+    if han >= 13:
+        child_score = 32000
+
+    return child_score + (child_score * dealer * 0.5)
 
 for dealer in (False, True):
     for fu in (list(range(20, 80, 2)) + [25]):
         print (fu, end = "\t")
         for han in range(1, 14):
-            print (f'{int(score(fu, han, dealer)):>5}', end = "\t")
+            print (f'{int(get_score(fu, han, dealer)):>5}', end = "\t")
         print ("")
     print ("")
